ac52c6286fc29561c74d4772e11358b66238850c
Aniket Sharma <aniketaz190@gmail.com>
2023-05-12 09:53:31 +0530

    Update Api.js

--- frontend-react/src/Api.js
+++ frontend-react/src/Api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+let baseURL = import.meta.env.PROD ? "https://calenderapp-production.up.railway.app" : "http://localhost:3000";
+
 const Api = axios.create({
-  baseURL: "http://localhost:3000",
-//   timeout: 1000,
-//   headers: { "X-Custom-Header": "foobar" },
+  baseURL,
+  //   timeout: 1000,
+  //   headers: { "X-Custom-Header": "foobar" },
 });
 
 export default Api;
